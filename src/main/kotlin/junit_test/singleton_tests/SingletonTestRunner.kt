6cee6b99b1ebb980d46f393697f199b5ed03c82b
Telle Africa <christelle-n.africa@gcash.com>
2022-05-22 16:00:40 +0800

    add logging to Singleton test runner

--- src/main/kotlin/junit_test/singleton_tests/SingletonTestRunner.kt
+++ src/main/kotlin/junit_test/singleton_tests/SingletonTestRunner.kt
@@ -1,27 +1,36 @@
 package junit_test.singleton_tests
 
+import junit_test.state_tests.StateTestRunner
 import org.junit.Assert.*
 import org.junit.Test
+import java.util.logging.Logger
 
 class SingletonTestRunner {
+
+    private val logger = Logger.getLogger(SingletonTestRunner::class.java.name)
+
     @Test
     fun helloIsTrueXXX1() {
         assertTrue(booleanMessage("Hello"))
+        logger.info("SingleObject is equal to the string.")
     }
 
     @Test
     fun helloIsFalseXXX1() {
         assertFalse(booleanMessage("Hello!"))
+        logger.info("SingleObject is not equal to the string.")
     }
 
     @Test
     fun returnsInstanceXXX1() {
         assertNotNull(objectExists())
+        logger.info("The object exists.")
     }
 
     @Test
     fun sameObjectXXX1() {
         assertTrue(sameObject())
+        logger.info("Same object returned.")
     }
 }
 
