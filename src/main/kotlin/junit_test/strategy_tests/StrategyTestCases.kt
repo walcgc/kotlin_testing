e8cf766ea4fdbde025bc14ee7ddaea5ca7abad73
Telle Africa <christelle-n.africa@gcash.com>
2022-05-22 11:14:37 +0800

    add testing for strategy design pattern

---
+++ src/main/kotlin/junit_test/strategy_tests/StrategyTestCases.kt
@@ -0,0 +1,39 @@
+package junit_test.strategy_tests
+
+import junit_test.strategy_tests.StrategyTestCases
+import java.util.logging.Level
+import java.util.logging.Logger
+
+object StrategyTestCases {
+    private val logger = Logger.getLogger(StrategyTestCases::class.java.name)
+    fun isCorrectResult(testResult: Int, referenceResult: Int): Boolean {
+
+        //set initial value to false
+        var result = false
+
+        //check if test value and reference value are equal
+        if (testResult == referenceResult) {
+            logger.log(Level.INFO, "The strategy was implemented correctly.")
+
+            //System.out.println("The strategy was implemented correctly.");
+            result = true
+        } else {
+            logger.log(Level.WARNING, "The strategy was NOT implemented correctly.")
+            //System.out.println("The strategy was NOT implemented correctly.");
+        }
+        //return result
+        return result
+    }
+
+    fun calculateReferenceSum(number1: Int, number2: Int): Int {
+        return number1 + number2
+    }
+
+    fun calculateReferenceDifference(number1: Int, number2: Int): Int {
+        return number1 - number2
+    }
+
+    fun calculateReferenceProduct(number1: Int, number2: Int): Int {
+        return number1 * number2
+    }
+}
---
+++ src/main/kotlin/junit_test/strategy_tests/StrategyTestRunner.kt
@@ -0,0 +1,77 @@
+package junit_test.strategy_tests
+
+import design_patterns.behavioral.strategy.Context
+import design_patterns.behavioral.strategy.Context.executeStrategy
+import junit_test.strategy_tests.StrategyTestCases
+import design_patterns.behavioral.strategy.strategies.OperationAdd
+import design_patterns.behavioral.strategy.strategies.OperationSubtract
+import design_patterns.behavioral.strategy.strategies.OperationMultiply
+import org.junit.Assert
+import org.junit.Test
+
+class StrategyTestRunner {
+    var number1 = 10
+    var number2 = 5
+    @Test
+    fun validateSum() {
+        val referenceResult = StrategyTestCases.calculateReferenceSum(number1, number2)
+        val context = Context(OperationAdd())
+        val testResult = context.executeStrategy(number1, number2)
+        Assert.assertEquals(testResult.toLong(), referenceResult.toLong())
+        Assert.assertTrue(StrategyTestCases.isCorrectResult(testResult, referenceResult))
+    }
+
+    @Test
+    fun validateDifference() {
+        val referenceResult = StrategyTestCases.calculateReferenceDifference(number1, number2)
+        val context = Context(OperationSubtract())
+        val testResult = context.executeStrategy(number1, number2)
+        Assert.assertEquals(testResult.toLong(), referenceResult.toLong())
+        Assert.assertTrue(StrategyTestCases.isCorrectResult(testResult, referenceResult))
+    }
+
+    @Test
+    fun validateProduct() {
+        val referenceResult = StrategyTestCases.calculateReferenceProduct(number1, number2)
+        val context = Context(OperationMultiply())
+        val testResult = context.executeStrategy(number1, number2)
+        Assert.assertEquals(testResult.toLong(), referenceResult.toLong())
+        Assert.assertTrue(StrategyTestCases.isCorrectResult(testResult, referenceResult))
+    }
+
+    @Test
+    fun testContextSwitching() {
+        val referenceResult =
+            StrategyTestCases.calculateReferenceProduct(number1, number2) - StrategyTestCases.calculateReferenceSum(
+                number1,
+                number2
+            )
+        var context = Context(OperationMultiply())
+        val testResult1 = context.executeStrategy(number1, number2)
+        context = Context(OperationAdd())
+        val testResult2 = context.executeStrategy(number1, number2)
+        context = Context(OperationSubtract())
+        val finalTestResult = context.executeStrategy(testResult1, testResult2)
+        Assert.assertEquals(finalTestResult.toLong(), referenceResult.toLong())
+        Assert.assertTrue(StrategyTestCases.isCorrectResult(finalTestResult, referenceResult))
+    }
+
+    @Test
+    fun testContextChaining() {
+        val referenceResult = StrategyTestCases.calculateReferenceProduct(
+            StrategyTestCases.calculateReferenceSum(number1, number2),
+            StrategyTestCases.calculateReferenceDifference(number1, number2)
+        )
+        val context = Context(OperationAdd())
+        //int testResult1 = context.executeStrategy(number1, number2);
+        val context2 = Context(OperationSubtract())
+        //int testResult2 = context2.executeStrategy(number1, number2);
+        val context3 = Context(OperationMultiply())
+        val finalTestResult = context3.executeStrategy(
+            context.executeStrategy(number1, number2),
+            context2.executeStrategy(number1, number2)
+        )
+        Assert.assertEquals(finalTestResult.toLong(), referenceResult.toLong())
+        Assert.assertTrue(StrategyTestCases.isCorrectResult(finalTestResult, referenceResult))
+    }
+}
